fa599c822ff32bbc19aaddd3f825d0072a8e8dd0
riderHimank <himankb500@gmail.com>
2024-02-08 23:55:33 +0530

    Screens2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navbar_login, Navbar_dashboard } from "./Navbar";
 import Hero from "./Hero";
 import { Cards_login, Cards_dashboard } from "./Cards";
+import Shop from "./Shop";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Hero />
       <Cards_login /> */}
       <Navbar_dashboard />
-      <Cards_dashboard />
+      {/* <Cards_dashboard /> */}
+      <Shop />
     </>
   );
 }
